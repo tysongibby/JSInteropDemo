43938c24e90cd0633662600d274e9d95fcd97e2e
Tyson Gibby <tyson.gibby@outlook.com>
2022-12-14 19:12:06 -0700

    Add project files.

---
+++ JSInteropDemo/wwwroot/js/interop.js
@@ -0,0 +1,22 @@
+﻿function createAlert() {
+    alert("Hey this is an alert");
+}
+
+function createPrompt(question) {
+    return prompt(question);
+}
+
+function setElementTextById(id, text) {
+    document.getElementById(id).innerText = text;
+}
+
+function focusOnElement(element) {
+    element.focus();
+}
+
+function giveMeRandomInt() {
+    DotNet.invokeMethodAsync('JSInteropDemo', 'GenerateRandomInt')
+        .then(result => {
+            setElementTextById('randomNumberSpan', result);
+        });
+}
